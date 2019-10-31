7939e74af223d095caf74a94f912c4df53ff4c53
Mike Prap <michaelpraplaski@yahoo.com>
2019-10-31 09:24:15 -0400

    Updates

---
+++ precache-manifest.0cedb475901282d5febc855a8a36dbaa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb61b8d55f6a1cd37424c24f3994bc6f",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "2d70778b551352d8fef3",
+    "url": "/Clicky-Game/static/css/main.6e345cc5.chunk.css"
+  },
+  {
+    "revision": "f189f686c3cfc58dc824",
+    "url": "/Clicky-Game/static/js/2.bd3a9e65.chunk.js"
+  },
+  {
+    "revision": "2d70778b551352d8fef3",
+    "url": "/Clicky-Game/static/js/main.e71873d5.chunk.js"
+  },
+  {
+    "revision": "515a30886e7d4bdd716c",
+    "url": "/Clicky-Game/static/js/runtime-main.49db83b4.js"
+  }
+]);
